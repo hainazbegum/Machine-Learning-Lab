169ed9154b7d16dfba22126c66134b8b2dab498f
Puppala Tejasri <122514294+hainazbegum@users.noreply.github.com>
2025-03-28 10:43:15 +0530

    Create finds OUTPUT.py

---
+++ finds OUTPUT.py
@@ -0,0 +1,16 @@
+The given training data set 
+
+['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same', 'Yes']
+The initial values of hypothesis 
+['Sunny', 'Warm', 'High', 'Strong', 'Warm', 'Same', 'Yes']
+The initial values of hypothesis 
+['Sunny', 'Cold', 'High', 'Strong', 'Warm', 'Change', 'No']
+The initial values of hypothesis 
+['Sunny', 'Warm', 'High', 'Strong', 'Cool', 'Change', 'Yes']
+The initial values of hypothesis 
+['0', '0', '0', '0', '0', '0']
+For training instance no: 0  the hypothesis is  ['Sunny', 'Warm', 'Normal', 'Strong', 'Warm', 'Same']
+For training instance no: 1  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', 'Warm', 'Same']
+For training instance no: 2  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', 'Warm', 'Same']
+For training instance no: 3  the hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
+The maximally specific hypothesis is  ['Sunny', 'Warm', '?', 'Strong', '?', '?']
